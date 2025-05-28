196f1c97f3075a1962c0a1bfe6404dd3d81af0b7
DADA6543 <dairo654@gmail.com>
2025-05-28 04:04:55 -0500

    Add files via upload

---
+++ domicilios_poli/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for domicilios_poli project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'domicilios_poli.settings')
+
+application = get_asgi_application()
