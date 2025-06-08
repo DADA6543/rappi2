d05bedfb730b646257cb0b36e18a87942fce2ab2
DADA6543 <dairo654@gmail.com>
2025-06-08 01:31:04 -0500

    Update settings.py

--- domicilios_poli/settings.py
+++ domicilios_poli/settings.py
@@ -84,8 +84,7 @@ WSGI_APPLICATION = 'domicilios_poli.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-   'default': dj_database_url.config(
-        default=os.environ.get('JAWSDB_URL')
+  'default': dj_database_url.parse(os.environ.get('JAWSDB_URL', 'sqlite:///db.sqlite3'))
     }
 }
 
