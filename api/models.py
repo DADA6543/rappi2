44921cd65811d500d2a0f9d7b13153bb7a937dd5
DADA6543 <dairo654@gmail.com>
2025-06-08 03:50:44 -0500

    Update models.py

--- api/models.py
+++ api/models.py
@@ -3,7 +3,9 @@ from django.contrib.auth.models import User
 
 class Producto(models.Model):
     nombre = models.CharField(max_length=100)
-    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    descripcion = models.TextField(blank=True)
+    precio = models.DecimalField(max_digits=8, decimal_places=2)
+    imagen_url = models.URLField(blank=True, null=True) 
 
     def __str__(self):
         return self.nombre
