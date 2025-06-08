091fc95a3ea517caa8f04d06fdd75c49a4d0c92e
DADA6543 <dairo654@gmail.com>
2025-06-08 04:51:50 -0500

    Update forms.py

--- web/forms.py
+++ web/forms.py
@@ -2,16 +2,10 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-class CustomRegisterForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-    confirm_password = forms.CharField(widget=forms.PasswordInput)
-    is_superuser = forms.BooleanField(required=False, label="¿Registrar como superusuario?")
-    superuser_code = forms.CharField(
-        required=False,
-        widget=forms.PasswordInput,
-        label="Clave de superusuario"
-    )
+class RegistroForm(UserCreationForm):
+    email = forms.EmailField(required=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+
