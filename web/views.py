c16aa48f085654e2e6b4e9b60e66fdbf75c26b4a
DADA6543 <dairo654@gmail.com>
2025-06-08 01:58:57 -0500

    Update views.py

--- web/views.py
+++ web/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .forms import RegistroForm
+from django.contrib.auth import login
+from django.shortcuts import redirect
 
 def home(request):
     return render(request, 'home.html')
@@ -26,3 +29,14 @@ def admin_utiles(request):
 
 def admin_ropa(request):
     return render(request, 'admin_ropa.html')
+
+def registro_view(request):
+    if request.method == 'POST':
+        form = RegistroForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Inicia sesión automáticamente
+            return redirect('/home/')  # Puedes redirigir al home u otra vista
+    else:
+        form = RegistroForm()
+    return render(request, 'registro.html', {'form': form})
